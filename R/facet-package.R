fa50f3614e8a168db1a0831e9aef9c06673a59ec
George Michaelides <g.michaelides@uea.ac.uk>
2026-04-11 00:27:15 +0100

    chore: add missing importFrom directives for dplyr, tidyr, ggplot2, rlang

--- R/facet-package.R
+++ R/facet-package.R
@@ -16,14 +16,15 @@
 #' @importFrom reformulas findbars
 #' @importFrom brms brm bf mvbind set_rescor set_prior default_prior prior prior_ prior_string empty_prior
 #' @importFrom tibble tibble as_tibble
-#' @importFrom dplyr filter mutate select bind_rows case_when
+#' @importFrom dplyr filter mutate select bind_rows case_when any_of arrange bind_cols c_across desc everything group_by relocate rowwise slice ungroup
 #' @importFrom magrittr %>%
-#' @importFrom tidyr pivot_longer
-#' @importFrom rlang as_name as_label is_formula
+#' @importFrom tidyr pivot_longer crossing pivot_wider
+#' @importFrom rlang as_name as_label is_formula .data
 #' @importFrom stats as.formula formula terms
 #' @importFrom methods is
 #' @importFrom ggplot2 ggplot aes geom_bar geom_col geom_errorbar labs theme_minimal coord_flip
 #' @importFrom ggplot2 geom_pointrange geom_hline theme element_text facet_wrap
+#' @importFrom ggplot2 geom_point geom_line ylim facet_grid
 #' @importFrom broom tidy glance
 "_PACKAGE"
 
